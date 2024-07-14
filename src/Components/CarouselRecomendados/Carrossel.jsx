dc20073a45153f867c5cb148c280aec9d8b3ecb9
karynnemoreira <karynnemoreira@yahoo.com.br>
2024-07-14 18:14:11 -0300

    carrossel e perfil

--- src/Components/CarouselRecomendados/Carrossel.jsx
+++ src/Components/CarouselRecomendados/Carrossel.jsx
@@ -1,39 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
-
-//componente de carrossel pra renderizar a imagem
-const Carrossel = ({imagens,titulo, children}) => {
-//configuração
-  const settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />
-  };
- //montagem do carrossel
-  return (
-    <div style={{padding: '60px', background: '#000' }}>
-      <h2 style={{ color: '#FFFF', marginBottom:"20px" }}>{titulo}</h2>
-      <Slider {...settings}> 
-        {children}
-      </Slider>
-    </div>
-  );
-}
-
-//personalização da seta
+// Personalização da seta
 const SampleNextArrow = (props) => {
   const { className, style, onClick } = props;
   return (
     <div
       className={className}
-      style={{ ...style, display: "block", background: "transparent", left:"88.5vw" }}
+      style={{ ...style, display: "block", background: "transparent", left: "88.5vw" }}
       onClick={onClick}
     />
   );
@@ -49,5 +25,43 @@ const SamplePrevArrow = (props) => {
     />
   );
 }
+
+const Carrossel = ({ imagens, titulo, children }) => {
+  const [slidesToShow, setSlidesToShow] = useState(4);
+
+  useEffect(() => {
+    const updateSlidesToShow = () => {
+      if (window.innerWidth <= 768) {
+        setSlidesToShow(1)
+      } else {
+        setSlidesToShow(4);
+      }
+    };
+
+    window.addEventListener('resize', updateSlidesToShow);
+    updateSlidesToShow();
+
+    return () => window.removeEventListener('resize', updateSlidesToShow);
+  }, []);
+
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: slidesToShow,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
+  };
+
+  return (
+    <div style={{ padding: '60px', background: '#000' }}>
+      <h2 style={{ color: '#FFFF', marginBottom: "20px" }}>{titulo}</h2>
+      <Slider {...settings}>
+        {children}
+      </Slider>
+    </div>
+  );
+};
 
 export default Carrossel;
